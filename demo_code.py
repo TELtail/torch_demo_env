374560bf60fe65c795c03a7b47a83749aa5bc474
macTEL <tyamamoto3141@gmail.com>
2022-12-06 17:22:03 +0900

    Demo code commit

---
+++ demo_code.py
@@ -0,0 +1,10 @@
+import torch
+
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
+
+print("Device is",device)
